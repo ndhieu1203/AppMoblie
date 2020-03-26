ee445fa3339cb7a89978e48c444cf1c095fb9936
= <=>
2020-03-26 22:47:37 +0700

    commit 2

---
+++ App/src/common/Styles.js
@@ -0,0 +1,21 @@
+const Styles = {
+    container: {
+        flexDirection: "column",
+        width: "100%",
+        height: "100%"
+    },
+    
+    top: {
+        flexDirection: "row",
+        flex: 1,
+        backgroundColor:"green"
+    },
+
+    bottom: {
+        flexDirection:"row",
+        flex: 9,
+        backgroundColor:"white"
+    }
+};
+
+export default Styles;
